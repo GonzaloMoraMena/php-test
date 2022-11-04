f49ed82e0e22e6c11ecb6ac65c5c94d9e0e46da2
Gonzalo Mora <ext_gfmoram@falabella.cl>
2022-11-04 18:40:36 -0300

    second

--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM lenguajes";
+$sql = "SELECT * FROM lenguajes;";
 $result = $conn->query($sql);
 echo '<h1> Modulo 3 Gonzalo Mora Conexión MySQL</h1>';
 if ($result->num_rows > 0) {
