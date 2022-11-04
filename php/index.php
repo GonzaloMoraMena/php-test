4dd6b0367431559ee727d38b17bd81ac0eaeca31
Gonzalo Mora <ext_gfmoram@falabella.cl>
2022-11-04 19:19:33 -0300

    second

--- php/index.php
+++ php/index.php
@@ -14,7 +14,7 @@ try
 
     $sql = 'SELECT name FROM modulo_3.lenguajes;';
     $result = $conn -> query ($sql);
-    var_dump($result); echo '<br>';
+     echo $result '<br>';
     echo '<h1> Modulo 3 Gonzalo Mora Conexión MySQL</h1>';
     if ($result->num_rows > 0) {
       echo '<h3>  Lenguajes de programación</h3></br></br>';
